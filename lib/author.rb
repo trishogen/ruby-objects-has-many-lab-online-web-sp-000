cdf62e4efe4a87e5bf4bf75f721825ca755dad21
Trish Ogen <patriciaogen@gmail.com>
2019-05-30 00:17:28 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,8 +2,6 @@ class Author
 
   attr_accessor :name, :posts
 
-  @@all_posts = []
-
   def initialize(name)
     @name = name
     @posts = []
@@ -11,19 +9,17 @@ class Author
 
   def add_post(post)
     @posts << post
-    @@all_posts << post
     post.author = self
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
-    @@all_posts << post
     post.author = self
   end
 
   def self.post_count
-    @@all_posts.size
+    Post.all.size
   end
 
 end
