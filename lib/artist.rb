c03428f30f14a0e9f8821b2e6a25ed6ca83c4b48
Trish Ogen <patriciaogen@gmail.com>
2019-05-30 00:18:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,6 @@ class Artist
 
   attr_accessor :name, :songs
 
-  @@all_songs = []
-
   def initialize(name)
     @name = name
     @songs = []
@@ -11,19 +9,17 @@ class Artist
 
   def add_song(song)
     @songs << song
-    @@all_songs << song
     song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
-    @@all_songs << song
     song.artist = self
   end
 
   def self.song_count
-    @@all_songs.size
+    Song.all.size
   end
 
 end
